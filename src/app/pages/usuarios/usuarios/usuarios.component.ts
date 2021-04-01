7b9172871893ffef641f0da51123ebd7f1eee0a7
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2021-03-31 22:43:29 -0600

    estilo a img

--- src/app/pages/usuarios/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios/usuarios.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -21,6 +21,10 @@ export class MenuService {
         {
           titulo: "Dashboard",
           url: "/deshboard"
+        },
+        {
+          titulo: "Usuarios",
+          url: "/usuarios"
         }
       ]
     }
